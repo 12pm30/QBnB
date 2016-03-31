01147817d067b83ee11be475f291f9949d7b7694
Ryan Baxter <ryan.baxter@queensu.ca>
2016-03-31 02:09:11 -0400

    Make account details load any account

--- accountdetailsload.php
+++ accountdetailsload.php
@@ -4,58 +4,68 @@
  ?>
 <?php
 if(isset($_SESSION['id'])){
-	include_once 'config/connection.php';
-	
-	$query = "SELECT first_name, middle_initial, last_name, email, primary_phone, secondary_phone, profile_pic_URL FROM member WHERE member_ID = ?";
-	
-	if ($stmt = $con->prepare($query)){
+	if (isset($_POST['member_ID_field'])){
+		include_once 'config/connection.php';
 		
-		$stmt->bind_Param("s", $_SESSION['id']);
-
-		$stmt->execute();
+		$query = "SELECT first_name, middle_initial, last_name, email, primary_phone, secondary_phone, profile_pic_URL FROM member WHERE member_ID = ?";
 		
-		$result = $stmt->get_result();
-		
-		$num = $result->num_rows;
-		
-		if ($num == 1){
-			$row = $result->fetch_assoc();
-			$resarray = array();
+		if ($stmt = $con->prepare($query)){
 			
-			$resarray[] = $row;	
-			echo json_encode($resarray);	
+			$stmt->bind_Param("s", $_POST['member_ID_field']);
+
+			$stmt->execute();
+			
+			$result = $stmt->get_result();
+			
+			$num = $result->num_rows;
+			
+			if ($num == 1){
+				$row = $result->fetch_assoc();
+				$resarraymd = array();
+				
+				$resarraymd[] = $row;
+			}
+			else {
+				echo "Error: Account not found.";
+				http_response_code(500);
+				die();
+			}
 		}
 		else {
-			echo "Error: Account not found.";
+			echo "SQL Prepare Failed. (Acct. Info)";
 			http_response_code(500);
+			die();
 		}
-	}
-	else {
-		echo "SQL Prepare Failed. (Acct. Info)";
-		http_response_code(500);
-	}
-	
-	$query = "SELECT year, faculty, type FROM degree WHERE member_ID = ?";
-	
-	if ($stmt = $con->prepare($query)){
 		
-		$stmt->bind_Param("s", $_SESSION['id']);
+		$query = "SELECT year, faculty, type FROM degree WHERE member_ID = ?";
+		
+		if ($stmt = $con->prepare($query)){
+			
+			$stmt->bind_Param("s", $_POST['member_ID_field']);
 
-		$stmt->execute();
-		
-		$result = $stmt->get_result();
+			$stmt->execute();
+			
+			$result = $stmt->get_result();
+			
+			$resarrayd = array();
+			
+			while($row = $result->fetch_assoc()){
+				$resarrayd[] = $row;	
+			}	
+		}
+		else {
+			echo "SQL Prepare Failed. (Deg. Info)";
+			http_response_code(500);
+			die();
+		}
 		
 		$resarray = array();
+		$resarray['details'] = $resarraymd;
+		$resarray['degrees'] = $resarrayd;
+		echo json_encode($resarray);
 		
-		while($row = $result->fetch_assoc()){
-			$resarray[] = $row;	
-		}
-		
-		echo json_encode($resarray);	
-	}
-	else {
-		echo "SQL Prepare Failed. (Deg. Info)";
-		http_response_code(500);
+		http_response_code(200);
+		die();
 	}
 }
 else {
@@ -63,3 +73,26 @@ else {
 	http_response_code(401);
 }
  ?>
+<!DOCTYPE HTML>
+<html>
+<body>
+ 
+<form name='detailsForm' id='detailsForm' action='accountdetailsload.php' method='post'>
+    <table border='0'>
+		<tr>
+			<td>Member ID</td>
+			<td><input type='number' name='member_ID_field' id='member_ID_field' 
+			 <?php if (isset($_POST['member_ID_field'])) echo 'value="'.$_POST['member_ID_field'].'"';?>
+			 /></td>
+		</tr>
+        <tr>
+            <td></td>
+            <td>
+                <input type='submit' id='lookupBtn' name='lookupBtn' value='Lookup Member' /> 
+            </td>
+        </tr>
+    </table>
+</form>
+ 
+</body>
+</html>
